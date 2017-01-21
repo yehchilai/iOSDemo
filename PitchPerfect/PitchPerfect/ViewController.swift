83bf953c960c16d04018000d56e4876f3ebabd94
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-21 15:03:43 -0800

    PitchPerfect - add: button and label with their function

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordAudio(_ sender: Any) {
+        print("Record button is pressed.")
+        recordingLabel.text = "Recording in progress..."
+    }
 
+    @IBAction func stopRecording(_ sender: UIButton) {
+        print("Stop recording button is pressed.")
+    }
 }
 
