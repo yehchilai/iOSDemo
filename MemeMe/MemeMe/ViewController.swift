5d93d33f9e64e758a1888132b43502dc5df09b1b
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-24 20:34:50 -0800

    MemeMe - add an image viewer and a toolbar. - update plist file for allowing to access Photo Library

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageVIew: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let controller = UIImagePickerController()
+        
+        present(controller, animated: true, completion: nil)
     }
-
 
 }
 
